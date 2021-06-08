fd84369b8b99cbd3dc28e201422174fb59903b64
miklakt <miklakt@gmail.com>
2021-06-08 16:42:13 +0200

    ClientBase class

--- socket_server/socket_server/Client.py
+++ socket_server/socket_server/Client.py
@@ -1,3 +1,4 @@
+from os import error
 import socket
 import pickle
 import time
@@ -6,29 +7,29 @@ import numpy
 from .Message import Message
 
 HEADER_LENGTH = 10
-    
-class Client():
+
+class ClientBase():
     _connected = False
-    last_message = None
-    system = None
-    busy = False 
+    logs = []
+    
     def __init__(self, IP, PORT) -> None:
         self.IP = IP
         self.PORT = PORT
 
     def connect(self):
         if self._connected:
-            pass #an error, do something
+            self.logs.append('Connection failed.')
+            return None
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.connect((self.IP, self.PORT))
         # Set connection to non-blocking state, so .recv() call won't block, just return some exception we'll handle
         #self.server_socket.setblocking(False)
         
-        print("Connecting to a server...")
+        self.logs.append("Connected to a server.")
         #The server will send an address it assigned to a new client
         addr = self._recv_object()
         self.addr = addr
-        print(f"Assigned addres: {self.addr}")
+        self.logs.append(f"Assigned addres: {self.addr}")
         self._connected = True
 
     def _send_object(self, data):
@@ -38,17 +39,86 @@ class Client():
 
     def _recv_object(self):
         message_header = self.server_socket.recv(HEADER_LENGTH)
-
         #client closed connection
-        if not len(message_header):
-            return False
-
+        if not len(message_header): return False
+        
         message_length = int(message_header.decode('utf-8').strip())
-
         serialized_data = self.server_socket.recv(message_length)
         data = pickle.loads(serialized_data)
-
         return data
+
+    def send_message(self, data, receiver):
+        msg = Message(data, sender = self.addr, receiver = receiver)
+        self._send_object(msg)
+
+    def recv_message(self):
+        msg = self._recv_object()
+        if msg is False: return False
+        if isinstance(msg, Message):
+            self.last_message = msg
+            if msg.sender == 'host':
+                self._income_host_message_handle(msg)
+            else:
+                self._income_client_message_handle(msg)
+            return msg
+        else:
+            self.logs.append("The data isn't wrapped into the Message class")
+            raise TypeError("The data isn't wrapped into the Message class")
+
+    def _income_client_message_handle(self, msg):
+        self.logs.append(f"[INCOME MESSAGE] from {msg.sender}")
+        data = msg.data
+        self.logs.append(data)
+
+    def _income_host_message_handle(self, msg):
+        print (f"[INCOME MESSAGE] from {msg.sender}")
+        data = msg.data
+        print (data)
+        if isinstance(data,str): 
+            if(data == '\STOP_SERVER'):
+                print('Server is going to be stopped.')
+                self._connected = False
+            if(data == '\ECHO'):
+                self.send_message('\ECHO_OK', 'host')
+
+    def echo_host(self):
+        self.send_message('\ECHO', 'host')
+        msg = self.recv_message()
+        if (msg.sender == 'host') & (msg.data == '\ECHO_OK'):
+            return True
+        else:
+            return False
+   
+    def _disconn_routine(self, sys_exit = True):
+        self.server_socket.close()
+        if sys_exit:
+            import sys
+            sys.exit()
+
+    def listen(self):
+        #server closed a connection
+        if self.recv_message() is False:
+            self.logs.append('Connection closed by the server')
+            self._connected = False
+            self._disconn_routine()
+
+    def flush_log(self):
+        while self.logs:
+            print(self.logs.pop(0))
+
+    def loop(self):
+        while self._connected:
+            self.listen()
+            self.flush_log()
+        else:
+            self._disconn_routine()
+
+
+class Client(ClientBase):
+    _connected = False
+    last_message = None
+    system = None
+    busy = False 
 
     def send_message(self, data, receiver):
         msg = Message(data, sender = self.addr, receiver = receiver)
@@ -100,30 +170,6 @@ class Client():
                 if not isinstance(data['exec'],list): data['exec'] = [data['exec']]
                 for item in data['exec']:
                     self.exec(item, 'host')       
-
-    def _disconn_routine(self, sys_exit = True):
-        self.server_socket.close()
-        if sys_exit:
-            import sys
-            sys.exit()
-
-    def listen(self):
-        #server closed a connection
-        if self.recv_message() is False:
-            print('Connection closed by the server')
-            self._disconn_routine()
-
-    def echo_test(self):
-        self.send_message('\ECHO', 'host')
-        self.recv_message()
-        
-
-    def loop(self):
-        while self._connected:
-            print(".")
-            self.listen()
-        else:
-            self._disconn_routine()
 
     def loop_thread(self, flavor='threading'):
         if flavor=='threading':
