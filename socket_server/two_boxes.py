f5e259830df069c946a1c96fc8700c65a3bd339c
ml <miklakt@gmail.com>
2021-06-16 01:38:14 +0300

    aggregate particles nmber, plot the change

--- socket_server/two_boxes.py
+++ socket_server/two_boxes.py
@@ -1,18 +1,17 @@
 #%%
-from os import wait
 import random
 import math
-import numpy
+import numpy as np
 
-import logging
-logging.basicConfig(stream=open('log', 'w'), level=logging.DEBUG)
+#import logging
+#logging.basicConfig(stream=open('log', 'w'), level=logging.DEBUG)
 
 from socket_server import Server
 
 server = Server('127.0.0.1', 10000)
 server.setup()
 server.start()
-#%%
+
 import subprocess
 clientA = subprocess.Popen(['python', 'esp_client.py'])
 clientB = subprocess.Popen(['python', 'esp_client.py'])
@@ -20,12 +19,12 @@ clientB = subprocess.Popen(['python', 'esp_client.py'])
 #wait for clients to connect
 while len(server.addr_list)<3:
     pass
-# %%
+
 #populate one of the boxes
 for i in range(100):
     server.request("self.system.part.add(pos=self.system.box_l * np.random.random(3))", 1, wait = False)
 server.request("len(self.system.part[:])", 1)
-# %%
+
 def get_energy():
     server.request("self.system.analysis.energy()['total']",0, wait = False)
     server.request("self.system.analysis.energy()['total']",1, wait = False)
@@ -117,9 +116,18 @@ def monte_carlo_move(beta):
         add_particle(sideA, old_pos)
 
 # %%
-
-# %%
-for i in range(100):
+plot_arr = []
+#%%
+#%%time
+for i in range(500):
     monte_carlo_move(beta = 1)
-    print(number_of_particles())
+    plot_arr.append(number_of_particles())
+#%%
+import matplotlib.pyplot as plt
+
+Y = np.array(plot_arr).T
+
+plt.plot(Y[0])
+plt.plot(Y[1])
+plt.show()
 # %%
