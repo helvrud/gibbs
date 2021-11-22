59b87765cb8ef1ef0eda25eaa022c913414f3352
miklakt <miklakt@gmail.com>
2021-11-22 20:25:15 +0100

    qsub factory

--- qsubs/qsub_factory.py
+++ qsubs/qsub_factory.py
@@ -10,22 +10,22 @@ def pbs_part(N, mem, ncpus, walltime, prefix):
         f'#PBS -o {prefix}/gibbs/data/{N}.qsubout\n',
     ))
 
-def singularity_exec(prefix, pypresso_docker, args):
+def singularity_exec(prefix, pypresso_docker, script_name, args):
     return ''.join((
         f"singularity exec ",
         f"{prefix}/ubuntu-gibbs.img {pypresso_docker} ",
-        f"{prefix}/gibbs/mcmd_polyelctrolyte/no_diamond_template.py ",
+        f"{prefix}/{script_name} ",
         ))+' '.join([str(v) for v in args])
 
-def generate(prefix, pypresso_docker, args, N=None, mem=500, ncpus=3, walltime="1:0:0"):
+def generate(prefix, pypresso_docker, script_name, args, N=None, mem=500, ncpus=3, walltime="1:0:0"):
     if N is None:
         N = '_'.join([str(v) for v in args]).replace('-','')
     with open(f"{N}.qsub", 'w') as f:
         f.write("#!/bin/bash\n")
         f.write(pbs_part(N, mem, ncpus, walltime, prefix))
-        f.write(singularity_exec(prefix, pypresso_docker, args))
+        f.write(singularity_exec(prefix, pypresso_docker, script_name, args))
 
 # %%
 args = ['-c_s', 0.1, '-gel_init_vol', 20000, '-v', 0.5, '-fixed_anions', 50]
-generate("/storage/praha1/home/laktionm", "/home/ml/espresso/espresso/es-build/pypresso", args)
+generate("/storage/praha1/home/laktionm", "/home/ml/espresso/espresso/es-build/pypresso", "gibbs/mcmd_polyelctrolyte/no_diamond_conc_as_arg.py", args)
 # %%
