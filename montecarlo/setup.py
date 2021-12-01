25e83e4aa1b25d6e5ea90955665a726f1df789a8
ml <miklakt@gmail.com>
2021-12-01 16:37:34 +0100

    vers increment

--- montecarlo/setup.py
+++ montecarlo/setup.py
@@ -6,7 +6,7 @@ HERE = pathlib.Path(__file__).parent
 
 setup(
     name='montecarlo',
-    version='0.0.1',
+    version='0.0.2',
     description='Abstract Monte Carlo and statistics',
     author='Laktionov Mikhail',
     author_email = 'miklakt@gmail.com',
