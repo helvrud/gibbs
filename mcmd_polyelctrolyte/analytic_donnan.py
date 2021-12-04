a3dd2f92564f329a444aae135055316092b81bd5
ml <miklakt@gmail.com>
2021-12-04 16:27:57 +0100

    anion cation were confused, add speciation of compressed

--- mcmd_polyelctrolyte/analytic_donnan.py
+++ mcmd_polyelctrolyte/analytic_donnan.py
@@ -26,11 +26,22 @@ def speciation(n_pairs, fixed_anions, v_salt, v_gel):
     return anion_salt, anion_gel, cation_salt, cation_gel
 
 def speciation_inf_reservoir(c_s, fixed_anions, v_gel):
-    cation_gel = gel_anions_inf_reservoir(c_s, fixed_anions, v_gel)
-    anion_gel = cation_gel+fixed_anions
+    anion_gel = gel_anions_inf_reservoir(c_s, fixed_anions, v_gel)
+    cation_gel = anion_gel+fixed_anions
     return anion_gel, cation_gel
+
+def speciation_compressed(c_s, fixed_anions, gel_initial_volume, v):
+    anion_gel_inf, cation_gel_inf = speciation_inf_reservoir(c_s, fixed_anions, gel_initial_volume)
+    gel_volume = gel_initial_volume*v
+    salt_volume = gel_initial_volume*(1-v)
+    return speciation(anion_gel_inf, fixed_anions, salt_volume, gel_volume)
 
 def zeta(n_pairs, fixed_anions, v_salt, v_gel):
     anion_salt, anion_gel, cation_salt, cation_gel = speciation(n_pairs, fixed_anions, v_salt, v_gel)
     zeta = anion_gel*v_salt/(anion_salt*v_gel)
+    return zeta
+
+def zeta_compressed(c_s, fixed_anions, gel_initial_volume, v):
+    anion_salt, anion_gel, cation_salt, cation_gel = speciation_compressed(c_s, fixed_anions, gel_initial_volume, v)
+    zeta = anion_gel*(1-v)/(anion_salt*v)
     return zeta
