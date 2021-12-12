12135000a3b849a3a9272e4c1a0462239fc3e5f6
ml <miklakt@gmail.com>
2021-12-12 21:04:39 +0100

    MCMD build routines moved to a separate script

--- mcmd_polyelctrolyte/ion_pair_monte_carlo.py
+++ mcmd_polyelctrolyte/ion_pair_monte_carlo.py
@@ -1,10 +1,9 @@
-# %%
 import time
 from typing import Tuple
 import numpy as np
 import random
 import logging
-logger = logging.getLogger(__name__)
+
 try:
     from tqdm import trange
 except:
@@ -14,11 +13,17 @@ from montecarlo import AbstractMonteCarlo
 from montecarlo import StateData, ReversalData, AcceptCriterion
 from sample_to_target import sample_to_target
 
+logger = logging.getLogger(__name__)
+
 SIDES = [0, 1]
 PAIR = [0, 1]
 CHARGES = [-1, 1]
 MOBILE_SPECIES = [0, 1]
 
+params = {" log_accept_reject" : False}
+
+def set_param(**kwargs):
+    params.update(kwargs)
 
 def _rotate_velocities_randomly(velocities):
     from scipy.spatial.transform import Rotation
@@ -28,7 +33,6 @@ def _rotate_velocities_randomly(velocities):
         rot = Rotation.random().as_dcm
     velocities_rotated = [list(rot().dot(velocity)) for velocity in velocities]
     return velocities_rotated
-
 
 def _entropy_change(anion_0, anion_1, cation_0, cation_1, volume_0, volume_1, removed_from=0):
     if removed_from == 0:
@@ -40,17 +44,16 @@ def _entropy_change(anion_0, anion_1, cation_0, cation_1, volume_0, volume_1, re
 class MonteCarloPairs(AbstractMonteCarlo):
 
     def __init__(self, server):
-        logger.info("Initializing...")
+        logger.info("Initializing Monte Carlo ion pairs exchange...")
         super().__init__()
         self.server = server
         self.setup()
-        logger.info("Initialization OK")
-
+        logger.info("Monte Carlo ion pairs exchange initialized")
 
     def setup(self) -> StateData:
         # request for energy, volume, mobile ions,
         # here we stay agnostic of immobile species
-        logger.debug("Retrieving data from tne nodes")
+        logger.debug("Retrieving data from tne espressomd  nodes...")
         request_body = [
             "potential_energy()",
             "system.box_l",
@@ -77,7 +80,7 @@ class MonteCarloPairs(AbstractMonteCarlo):
         )
 
         self.current_state = new_state
-        logger.debug("Node states are updated")
+        logger.debug("New energy, volume and speciation are retrieved from espressomd nodes")
         return new_state
 
     def move(self) -> Tuple[ReversalData, AcceptCriterion]:
@@ -184,7 +187,19 @@ class MonteCarloPairs(AbstractMonteCarlo):
             for i in PAIR
         ], other_side)
 
+    def on_accept(self):
+        if params[' log_accept_reject'] == True:
+            logger.info("Accepted")
+        return super().on_accept()
+
+    def on_reject(self):
+        if params[' log_accept_reject'] == True:
+            logger.info("Accepted")
+        return super().on_reject()
+
     # AUXILIARY
+    # some minor extra calculation
+
     def entropy_change(self, side):
         volume = self.current_state.volume
         anion = self.current_state.anions
@@ -198,7 +213,11 @@ class MonteCarloPairs(AbstractMonteCarlo):
         zeta = float((anion_gel*volume_salt)/(anion_salt*volume_gel))
         return zeta
 
+
     # SAMPLING FUNCTIONS
+    # function to sample autocorrelated variables
+    # *kwargs are
+
     def sample_zeta_to_target_error(self, **kwargs):
         def get_zeta_callback(sample_size):
             zetas = []
@@ -259,7 +278,10 @@ class MonteCarloPairs(AbstractMonteCarlo):
             'err': err,
             'sample_size': sample_size
         }
+
+
     # MD functions
+    # request connected nodes to do some MD and espressomd specific jobs
 
     def run_md(self, md_steps):
         self.server(f"system.integrator.run({md_steps})", [0, 1])
@@ -296,184 +318,3 @@ class MonteCarloPairs(AbstractMonteCarlo):
                     f"populate({count}, **{PARTICLE_ATTR[species]})", i)
         self.setup()
         return True
-
-################################################################################
-
-
-def build_no_gel(
-    Volume, N_pairs, fixed_anions,
-    electrostatic=False,
-    no_interaction=False,
-    python_executable='python',
-    script_name = 'espresso_nodes/run_node.py',
-    args=[[],[]]
-):
-    import socket_nodes
-
-    # box volumes and dimmensions
-    box_l = [V_**(1/3) for V_ in Volume]
-
-    # start server and nodes
-    server = socket_nodes.utils.create_server_and_nodes(
-        scripts=[script_name]*2,
-        args_list=[
-            [
-                '-l', box_l[0], '--salt',
-            ]+(['--no_interaction'] if no_interaction else []) + args[0],
-            [
-                '-l', box_l[1], '--salt',
-            ]+(['--no_interaction'] if no_interaction else []) + args[1],
-        ],
-        python_executable=python_executable,
-        #stdout=subprocess.PIPE,
-        #stderr=subprocess.PIPE,
-    )
-    # simulate polyelectrolyte without diamond structure
-    from espresso_nodes.shared import PARTICLE_ATTR
-    # add counterions
-    server(f"populate({fixed_anions}, **{PARTICLE_ATTR['cation']})", 1)
-    # add fixed anions
-    server(f"populate({fixed_anions}, **{PARTICLE_ATTR['gel_anion']})", 1)
-
-    MC = MonteCarloPairs(server)
-
-    MC.populate(N_pairs)
-
-    server("minimize_energy()", [0,1])
-
-    if electrostatic:
-        print("Electrostatic is set on")
-        server('enable_electrostatic()', [0, 1])
-        print("Electrostatic is enabled")
-
-    return MC
-
-
-def build_no_gel_salinity(
-    c_s_mol, gel_initial_volume, v, **kwargs
-):
-    from utils import mol_to_n
-    from analytic_donnan import speciation, speciation_inf_reservoir
-    c_s = mol_to_n(c_s_mol, unit_length_nm=0.35)
-    fixed_anions = kwargs["fixed_anions"]
-    anions_inf_res, cations_inf_res = speciation_inf_reservoir(
-        c_s, fixed_anions, gel_initial_volume)
-    gel_volume = gel_initial_volume*v
-    salt_volume = gel_initial_volume*(1-v)
-    anion_salt, anion_gel, cation_salt, cation_gel = map(
-        round,
-        speciation(anions_inf_res, fixed_anions, salt_volume, gel_volume)
-    )
-    print('Whithin donnan theory:\t',
-        f"anion_salt: {anion_salt}",
-        f"anion_gel: {anion_gel}",
-        f"cation_salt: {cation_salt}",
-        f"cation_gel: {cation_gel}")
-    MC = build_no_gel(
-        Volume=[salt_volume, gel_volume],
-        N_pairs=[anion_salt, anion_gel],
-        **kwargs)
-    return MC
-
-################################################################################
-def build_gel(
-    Volume, N_pairs, fixed_anions, MPC, bond_length,
-    electrostatic=False,
-    no_interaction=False,
-    python_executable='python',
-    script_name = 'espresso_nodes/run_node.py',
-    args=[[],[]]
-):
-    import socket_nodes
-    try:
-        socket_nodes.set_params(LOG_REQUESTS_INFO = False)
-    except:
-        pass
-
-    n_gel_part = MPC*16+8
-    # box volumes and dimmensions
-    box_l = [V_**(1/3) for V_ in Volume]
-
-    # start server and nodes
-    server = socket_nodes.utils.create_server_and_nodes(
-        scripts=[script_name]*2,
-        args_list=[
-            [
-                '-l', box_l[0], '--salt',
-            ]+(['--no_interaction'] if no_interaction else []) + args[0],
-            [
-                '-l', box_l[1], '--gel', '-MPC', MPC,
-                '-bond_length', bond_length, '-alpha', fixed_anions/n_gel_part
-            ]+(['--no_interaction'] if no_interaction else []) + args[1],
-        ],
-        python_executable=python_executable,
-        connection_timeout_s = 1200
-        #stdout=subprocess.PIPE,
-        #stderr=subprocess.PIPE,
-    )
-
-    MC = MonteCarloPairs(server)
-
-    MC.populate(N_pairs)
-
-    server("minimize_energy()", [0,1])
-
-    if electrostatic:
-        print("Electrostatic is set on")
-        server('enable_electrostatic()', [0, 1])
-        print("Electrostatic is enabled")
-
-    return MC
-
-def build_gel_salinity(
-    c_s_mol, gel_initial_volume, v, **kwargs
-):
-    from utils import mol_to_n
-    from analytic_donnan import speciation, speciation_inf_reservoir
-    #particles per sigma^3
-    c_s = mol_to_n(c_s_mol, unit_length_nm=0.35)
-    fixed_anions = kwargs["fixed_anions"]
-    #'soak' gel in inf reservoir
-    anions_inf_res = round(speciation_inf_reservoir(
-        c_s, fixed_anions, gel_initial_volume)[0])
-
-    gel_volume = gel_initial_volume*v
-    salt_volume = gel_initial_volume*(1-v)
-
-    anion_salt, anion_gel, cation_salt, cation_gel = map(
-        round,
-        speciation(anions_inf_res, fixed_anions, salt_volume, gel_volume)
-    )
-    print('Whithin donnan theory:\t',
-        f"anion_salt: {anion_salt}",
-        f"anion_gel: {anion_gel}",
-        f"cation_salt: {cation_salt}",
-        f"cation_gel: {cation_gel}")
-    MC = build_gel(
-        Volume=[salt_volume, gel_volume],
-        N_pairs=[anion_salt, anion_gel],
-        **kwargs)
-    return MC
-
-def build_gel_n_pairs(
-    n_pairs_all, gel_initial_volume, v, **kwargs
-    ):
-    from analytic_donnan import speciation
-    fixed_anions = kwargs["fixed_anions"]
-    gel_volume = gel_initial_volume*v
-    salt_volume = gel_initial_volume*(1-v)
-    anion_salt, anion_gel, cation_salt, cation_gel = map(
-        round,
-        speciation(n_pairs_all, fixed_anions, salt_volume, gel_volume)
-    )
-    print('Started with {n_pairs} pairs')
-    print('Whithin donnan theory:\t',
-        f"anion_salt: {anion_salt}",
-        f"anion_gel: {anion_gel}",
-        f"cation_salt: {cation_salt}",
-        f"cation_gel: {cation_gel}")
-    MC = build_gel(
-        Volume=[salt_volume, gel_volume],
-        N_pairs=[anion_salt, anion_gel],
-        **kwargs)
-    return MC
