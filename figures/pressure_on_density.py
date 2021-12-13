eae4f03f9bedfbfd1b17bef8c70f095e377bc590
miklakt <miklakt@gmail.com>
2021-12-13 20:33:55 +0100

    pressure graph

--- figures/pressure_on_density.py
+++ figures/pressure_on_density.py
@@ -7,10 +7,9 @@ df = pd.read_pickle('../data/gel_all_data.pkl')
 df.sort_values(by = 'v', inplace = True)
 #%%
 g =None
-page = None
 for (idx, group), color in zip(df.groupby(by = 'n_pairs'), color_cycle):
         g=vplot(
-            list(group.gel_density), list(group.c_s_mol_mean),
+            list(group.gel_density), [list(group.pressure_gel_mean - group.pressure_salt_mean), list(group.pressure_gel_err + group.pressure_salt_err)],
             xname = 'x'+str(idx), yname = 'y'+str(idx),
             g=g, color = color
             )
