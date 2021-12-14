4266d79304d4a4e8ee51826b53418b79c96791b7
ml <miklakt@gmail.com>
2021-12-14 22:42:32 +0100

    delta P pascals

--- process_data.py
+++ process_data.py
@@ -1,7 +1,6 @@
 #%%
 import numpy as np
 import pickle
-from numpy.core.fromnumeric import mean
 import pandas as pd
 import pathlib
 
@@ -41,6 +40,7 @@ def mean_and_err(column_name, df):
             pass
 
 def split_2d_arrays_in_columns(column_name, df, postfix = None, replace = True):
+    df[column_name]=df[column_name].apply(np.array)
     if postfix is None:
         indices = range(np.shape(df[column_name][0])[1])
         for i in indices:
@@ -71,17 +71,29 @@ salt_gel_columns = ['anion', 'cation', 'pressure']
 for col in salt_gel_columns: df[col] = df[col].apply(np.array)
 
 columns_to_rename = {
-    'input_gel_initial_volume' : 'V0', 
-    'input_n_pairs_all' : 'n_pairs', 
+    'input_gel_initial_volume' : 'V0',
+    'input_n_pairs_all' : 'n_pairs',
     'input_fixed_anions' : 'fixed_anions',
-    'input_v' : 'v', 
-    'input_MPC' : 'MPC', 
-    'input_electrostatic' : 'electrostatic', 
+    'input_v' : 'v',
+    'input_MPC' : 'MPC',
+    'input_electrostatic' : 'electrostatic',
 }
 
 df.rename(columns = columns_to_rename, inplace=True)
 
+#from grand canonical ensemble calculation
+'''
+pCl	    \cs	    V       box_l   na^{gel}    cl^{gel}    v_5bar
+0.40	0.5702	1.7337	32.0	845.7	    365.8
+0.82	0.2083	3.1385	39.0	709.0	    229.1       0.26?
+1.00	0.1308	3.2607	39.5	617.7	    137.8       0.30
+1.30	0.0617	4.2066	43.0	550.9	    70.9        0.27
+2.00	0.0111	7.2268	51.5	490.8	    10.8        0.20
+2.22	0.0065	7.8768	53.0	484.7	    4.68        0.15
+'''
+
 n_pairs_c_s_mol = {
+    578 : 0.2762,
     229 : 0.2083,
     138 : 0.1308,
     71 : 0.0617,
@@ -92,7 +104,6 @@ def f(x):
     if x in n_pairs_c_s_mol: return n_pairs_c_s_mol[x]
     else: return None
 df['c_s_reservoir_mol'] = df.n_pairs.apply(f)
-#%%
 # %%
 for col in salt_gel_columns:
     try:
@@ -100,11 +111,12 @@ for col in salt_gel_columns:
     except Exception as e:
         print(e)
 #%%
-from mcmd_polyelctrolyte.utils import n_to_mol
+from mcmd_polyelctrolyte.utils import n_to_mol, pressure_to_Pa
 df['volume_gel'] = df.v*df.V0
 df['volume_salt'] = (1-df.v)*df.V0
 df['c_s_mol'] = df.apply(lambda _: n_to_mol(_.anion_salt/_.volume_salt), axis =1)
 df['gel_density'] = df.apply(lambda _: n_to_mol((_.MPC*16+8)/_.volume_salt), axis =1)
+df['delta_P_Pa'] =  df.apply(lambda _: pressure_to_Pa(_.pressure_gel - _.pressure_salt), axis =1)
 #%%
 for col in df.columns:
     try:
@@ -113,4 +125,3 @@ for col in df.columns:
         pass
 #%%
 df.to_pickle(pickle_path)
-
