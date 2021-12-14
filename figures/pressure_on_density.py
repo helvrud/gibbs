80839eab019445a7cc1dcf598ed8f7f551babd62
ml <miklakt@gmail.com>
2021-12-14 22:41:15 +0100

    pascals

--- figures/pressure_on_density.py
+++ figures/pressure_on_density.py
@@ -9,7 +9,7 @@ df.sort_values(by = 'v', inplace = True)
 g =None
 for (idx, group), color in zip(df.groupby(by = 'n_pairs'), color_cycle):
         g=vplot(
-            list(group.gel_density), [list(group.pressure_gel_mean - group.pressure_salt_mean), list(group.pressure_gel_err + group.pressure_salt_err)],
+            list(group.gel_density), [list(-group.delta_P_Pa_mean), list(group.delta_P_Pa_err)],
             xname = 'x'+str(idx), yname = 'y'+str(idx),
             g=g, color = color
             )
