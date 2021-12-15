7f01aa88e41894bc843e49d192d297e7ca3a45af
O.V. Rud <helvrud@gmail.com>
2021-12-15 15:51:20 +0100

    edited figurees

--- figures/pressure_on_density.py
+++ figures/pressure_on_density.py
@@ -7,9 +7,13 @@ df = pd.read_pickle('../data/gel_all_data.pkl')
 df.sort_values(by = 'v', inplace = True)
 #%%
 g =None
+
+df['delta_P_bar_mean'] = df.delta_P_Pa_mean * 1e-5
+df['delta_P_bar_err'] = df.delta_P_Pa_err * 1e-5
+
 for (idx, group), color in zip(df.groupby(by = 'n_pairs'), color_cycle):
         g=vplot(
-            list(group.gel_density), [list(group.delta_P_Pa_mean), list(group.delta_P_Pa_err)],
+            list(group.gel_density), [list(group.delta_P_bar_mean), list(group.delta_P_bar_err)],
             xname = 'x'+str(idx), yname = 'y'+str(idx),
             g=g, color = color
             )
