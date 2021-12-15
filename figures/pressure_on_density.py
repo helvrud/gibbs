64fd51b30be5196f49f5ec4802c4b0f4cce8cb2f
O.V. Rud <helvrud@gmail.com>
2021-12-15 18:54:53 +0100

    edited text

--- figures/pressure_on_density.py
+++ figures/pressure_on_density.py
@@ -12,15 +12,21 @@ df['delta_P_bar_mean'] = df.delta_P_Pa_mean * 1e-5
 df['delta_P_bar_err'] = df.delta_P_Pa_err * 1e-5
 global xy, x_axis, y_axis, x_data, y_data, x_dataname, y_dataname
 for (idx, group), color in zip(df.groupby(by = 'n_pairs'), color_cycle):
-        [g, graph, xy]=vplot(
-            list(group.gel_density), [list(group.delta_P_bar_mean), list(group.delta_P_bar_err)],
-            xname = 'x'+str(idx), yname = 'y'+str(idx),
-            g=g, color = color
-            )
-        xy.key.val = 'c_{s} = '+str(float(group.c_s_reservoir_mol.head(1)))
-        key = graph.Add('key')
-        g.Root.page1.graph1.x.label.val = 'hydrogel density, \\varphi, [mol/l]'
-        g.Root.page1.graph1.y.label.val = '\\Delta P = P - P_{res}, [bar]'
+    [g, graph, xy]=vplot(
+        list(group.gel_density), [list(group.delta_P_bar_mean), list(group.delta_P_bar_err)],
+        xname = 'x'+str(idx), yname = 'y'+str(idx), xlog = True,
+        g=g, color = color
+        )
+    xy.key.val = 'c_{s} = '+str(float(group.c_s_reservoir_mol.head(1)))
+graph.y.max.val=5
+graph.y.min.val=0
+key = graph.Add('key')
+key.vertPosn.val = 'top'
+key.horzPosn.val = 'left'
+key.Border.hide.val  = True
+g.Root.page1.graph1.x.label.val = 'hydrogel density, \\varphi, [mol/l]'
+g.Root.page1.graph1.y.label.val = '\\Delta P = P - P_{res}, [bar]'
+
 #%%
 g.Save(__file__.replace('.py','.vsz'))
 #%%
