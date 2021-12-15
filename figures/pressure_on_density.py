5d77bca840a29c521654bc7da24e27cba1cbcb36
ml <miklakt@gmail.com>
2021-12-15 15:40:07 +0100

    negative pressure fix

--- figures/pressure_on_density.py
+++ figures/pressure_on_density.py
@@ -9,7 +9,7 @@ df.sort_values(by = 'v', inplace = True)
 g =None
 for (idx, group), color in zip(df.groupby(by = 'n_pairs'), color_cycle):
         g=vplot(
-            list(group.gel_density), [list(-group.delta_P_Pa_mean), list(group.delta_P_Pa_err)],
+            list(group.gel_density), [list(group.delta_P_Pa_mean), list(group.delta_P_Pa_err)],
             xname = 'x'+str(idx), yname = 'y'+str(idx),
             g=g, color = color
             )
