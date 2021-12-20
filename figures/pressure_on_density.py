cdb326c32359d5a9464acadf0fee560829cca89f
miklakt <miklakt@gmail.com>
2021-12-20 17:03:11 +0100

    figures update

--- figures/pressure_on_density.py
+++ figures/pressure_on_density.py
@@ -18,8 +18,10 @@ df['delta_P_bar_err'] = df.delta_P_Pa_err * 1e-5
 global xy, x_axis, y_axis, x_data, y_data, x_dataname, y_dataname
 for (idx, group), color in zip(df.groupby(by = 'n_pairs'), color_cycle):
     [g, graph, xy]=vplot(
-        list(group.gel_density), [list(group.delta_P_bar_mean), list(group.delta_P_bar_err)],
-        xname = 'x'+str(idx), yname = 'y'+str(idx), xlog = True,
+        list(group.gel_density), 
+        [list(group.delta_P_bar_mean), list(group.delta_P_bar_err)],
+        xname = 'x'+str(idx), yname = 'y'+str(idx), 
+        xlog = True, ylog = True,
         g=g, color = color
         )
     xy.key.val = 'c_{s} = '+str(float(group.c_s_reservoir_mol.head(1)))
