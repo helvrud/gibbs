4f31e44ba634cd4f5d315681c47dc24941d5f2f7
ml <miklakt@gmail.com>
2022-02-07 21:47:15 +0300

    one step desalination cycle

--- desalination_cycle.py
+++ desalination_cycle.py
@@ -14,7 +14,7 @@ plt.rcParams['axes.ymargin'] = 0
 from mcmd_polyelctrolyte.utils import n_to_mol
 
 gibbs_data_path = "data/gel_all_data.pkl"
-gc_data_path = "data/GC.data"
+gc_data_path = "data/GC.pkl"
 
 gibbs_raw = pd.read_pickle(gibbs_data_path)
 gc_raw = pd.read_pickle(gc_data_path)
@@ -47,20 +47,36 @@ gc_raw.P = gc_raw.P.apply(lambda _: _[0])
 #approx n_pairs used in further gibbs ensemble
 gc_raw['n_pairs'] = round(gc_raw.Ncl_eq)
 gc_raw['n_pairs'] = gc_raw['n_pairs'].astype(int)
+gc_raw.V = gc_raw.box_l**3
 # %%
+gc_raw = gc_raw.sort_values(by = "Ncl_eq")
+#%%
+#gc_raw.c_s_rounded = gc_raw.cs.apply(round)
+gc_to_plot = gc_raw.iloc[2]
+gibbs_to_plot = gibbs_pivot.iloc[[2,10]]
 
 #%%
 fig, ax = plt.subplots()
 #ax.set_yscale('log')
 #ax.set_xscale('log')
-for idx, grouped in gibbs_pivot.groupby(by = ['n_pairs', 'V0', 'fixed_anions']):
-    ax.scatter(list(grouped.V), list(grouped.P), label = idx[0])
-ax.legend(title = 'n_pairs')
-# %%
-fig, ax = plt.subplots()
+#for idx, grouped in gibbs_to_plot.groupby(by = ['n_pairs']):
+#    ax.scatter(grouped.V.squeeze(), grouped.P.squeeze(), label = idx[0], marker = "s")
+#ax.legend(title = 'n_pairs')
+for idx, row in gibbs_to_plot.iterrows():
+    ax.scatter(row.V, np.array(row.P)*1e-5, label = "gibbs "+ str(row.cs[0]), marker = "s")
+#fig.savefig("gibbsPV.pdf")
+
+#fig, ax = plt.subplots()
 #ax.set_yscale('log')
 #ax.set_xscale('log')
-for idx, grouped in gc_raw.groupby(by = ['n_pairs']):
-    ax.scatter(list(grouped.V), list(grouped.P), label = idx)
-ax.legend(title = 'n_pairs')
+#for idx, grouped in gc_to_plot.groupby(by = ['Ncl_eq']):
+#    try:
+#        ax.scatter(grouped.V.squeeze(), grouped.P.squeeze(), label = idx)
+#    except:
+#        pass
+ax.scatter(gc_to_plot.T.V.squeeze(), gc_to_plot.T.P.squeeze(), label = "GC " + str(gc_to_plot.T.cs.squeeze()))
+ax.set_ylim((-1,5))
+ax.axhline(y =0, color = "black")
+ax.legend(title = 'cs')
+fig.savefig("gcPV.pdf")
 # %%
